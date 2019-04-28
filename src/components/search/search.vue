0dfa75376cc4f4ff5312813cbcaf86bd015ba3f1
WenFeiTongXue <rubbish_Z@me.com>
2019-04-28 21:33:39 +0800

    骨架

---
+++ src/components/search/search.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    搜索
+  </div>
+</template>
+<script>
+export default {
+  data(){return {}}
+}
+</script>
+<style scoped>
+
+</style>
+
--- src/router.js
+++ src/router.js
@@ -2,12 +2,20 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Player from './views/player.vue'
+import Recommend from "./components/recommend/recommend.vue"
+import Singer from "./components/singer/singer.vue"
+import Rank from "./components/rank/rank.vue"
+import Search from "./components/search/search.vue"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {path: '/', component: Home },
+    {path: '/recommend', component: Recommend },
+    {path: '/singer', component: Singer },
+    {path: '/rank', component: Rank },
+    {path: '/search', component: Search },
     {path: '/player', component: Player },
   ]
 })
